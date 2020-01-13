8a4289c2b05273ff711d97d1d90e00811592ac14
darkwolfxj <hesitation.dotdot@gmail.com>
2020-01-13 17:29:04 -0500

    finished cta

--- js/index.js
+++ js/index.js
@@ -41,7 +41,7 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-
+//nav
 let thenav = Array.from(document.querySelectorAll('nav a'));
 console.log(thenav);
 thenav[0].textContent=siteContent.nav["nav-item-1"];
@@ -49,46 +49,10 @@ thenav[1].textContent=siteContent.nav["nav-item-2"];
 thenav[2].textContent=siteContent.nav["nav-item-3"];
 thenav[3].textContent=siteContent.nav["nav-item-4"];
 thenav[4].textContent=siteContent.nav["nav-item-5"];
-thenav[5].textContent=siteContent.nav["nav-item-6"]
+thenav[5].textContent=siteContent.nav["nav-item-6"];
 
+//cta
+document.querySelector('.cta .cta-text h1').textContent=siteContent.cta["h1"];
+document.querySelector('.cta button').textContent=siteContent.cta["button"];
+document.querySelector('.cta img').src=siteContent.cta["img-src"];
 
-
-// //nav
-// let nav = document.querySelector('header nav'); //selecting nav in the DOM
-
-// let nav1 = document.querySelector('nav a'); //selecting first link in the DOM
-// nav1.textContent=siteContent.nav["nav-item-1"];
-
-// nav.prepend(nav1);
-// console.log(nav1);
-// console.log('is nav1')
-
-// let nav2 = document.querySelector('nav a:nth-child(2)');  //selecting second link in the DOM
-// nav2.textContent=siteContent.nav["nav-item-2"];
-// nav.append(nav2);
-// console.log(nav2);
-// console.log('is nav2')
-
-// let nav3 = document.querySelector('nav a:nth-child(3)');  //selecting third link in the DOM
-// nav3.textContent=siteContent.nav["nav-item-3"];
-// nav.append(nav3);
-// console.log(nav3);
-// console.log('is nav3')
-
-// let nav4 = document.querySelector('nav a:nth-child(4)');  //selecting fourth link in the DOM
-// nav4.textContent=siteContent.nav["nav-item-4"];
-// nav.append(nav4);
-// console.log(nav4);
-// console.log('is nav4')
-
-// let nav5 = document.querySelector('nav a:nth-child(5)'); //selecting fifth link in the DOM
-// nav5.textContent=siteContent.nav["nav-item-5"];
-// nav.append(nav5);
-// console.log(nav5);
-// console.log('is nav5')
-
-// let nav6 = document.querySelector('nav a:nth-child(6)'); //selecting sixth link in the DOM
-// nav6.textContent=siteContent.nav["nav-item-6"];
-// nav.append(nav6);
-// console.log(nav6);
-// console.log('is nav6');
